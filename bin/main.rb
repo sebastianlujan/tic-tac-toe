84aa62ac5bda3a8abdd4391b25bfa0155359bfce
aracelicaes <aracelicaes@gmail.com>
2020-02-24 16:48:29 -0600

    Add: pseudo code for game org

---
+++ bin/main.rb
@@ -0,0 +1,16 @@
+puts 'Hello world!'
+puts 'Hello world!'
+X is always COMPUTER
+0 is always Player
+Prompt 'Welcome! Tic Tac Toe to the Death!'
+Display Board
+Prompt: Write a number from 1-9
+USER Writes #
+Get user Input
+if valid input check if input is a valid place (not OCUPIED)
+Ask for next input
+Validate input
+Repeat this until all positions are occupied
+or...positions
+continue?
+Tie, Win, Loose = End of Game
